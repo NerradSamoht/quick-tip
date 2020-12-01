2a04c834ee03fe1cd1d64b0e3827276e87dfbb60
Darren Thomas <bittledroid@gmail.com>
2020-12-01 14:50:51 +0000

    Convert indents to spaces (sorry lads)

--- tip-calculator/scripts.js
+++ tip-calculator/scripts.js
@@ -2,30 +2,30 @@ let container = document.querySelector("#container");
 container.addEventListener('input', update);
 
 function formatMoney(value) {
-	value = Math.ceil(value * 100) / 100;
-	value = value.toFixed(2);
-	return "£ " + value;
+  value = Math.ceil(value * 100) / 100;
+  value = value.toFixed(2);
+  return "£ " + value;
 }
 
 function formatSplit(value) {
-	if (value === "1") return value + " person";
-	return value + " people";
+  if (value === "1") return value + " person";
+  return value + " people";
 }
 
 function update() {
-	let bill = Number(document.querySelector('#yourBill').value);
-	let tipPercent = document.querySelector('#tipInput').value;
-	let split = document.querySelector('#splitInput').value;
+  let bill = Number(document.querySelector('#yourBill').value);
+  let tipPercent = document.querySelector('#tipInput').value;
+  let split = document.querySelector('#splitInput').value;
 
-	let tipValue = bill * (tipPercent / 100);
-	let tipEach = tipValue / split;
-	let newBillEach = (bill + tipValue) / split;
+  let tipValue = bill * (tipPercent / 100);
+  let tipEach = tipValue / split;
+  let newBillEach = (bill + tipValue) / split;
 
-	document.querySelector('#tipPercent').innerHTML = tipPercent + "%";
-	document.querySelector('#tipValue').innerHTML = formatMoney(tipValue);
-	document.querySelector('#totalWithTip').innerHTML = formatMoney(bill + tipValue);
-	document.querySelector('#splitValue').innerHTML = formatSplit(split);
-	document.querySelector('#billEach').innerHTML = formatMoney(newBillEach);
-	document.querySelector('#tipEach').innerHTML = formatMoney(tipEach);
+  document.querySelector('#tipPercent').innerHTML = tipPercent + "%";
+  document.querySelector('#tipValue').innerHTML = formatMoney(tipValue);
+  document.querySelector('#totalWithTip').innerHTML = formatMoney(bill + tipValue);
+  document.querySelector('#splitValue').innerHTML = formatSplit(split);
+  document.querySelector('#billEach').innerHTML = formatMoney(newBillEach);
+  document.querySelector('#tipEach').innerHTML = formatMoney(tipEach);
 
 }
