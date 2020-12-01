f30907d01338b661c9cb3fd785d4bf631b8a7e67
Darren Thomas <bittledroid@gmail.com>
2020-12-01 14:48:08 +0000

    Add JS functionality

---
+++ tip-calculator/scripts.js
@@ -0,0 +1,31 @@
+let container = document.querySelector("#container");
+container.addEventListener('input', update);
+
+function formatMoney(value) {
+	value = Math.ceil(value * 100) / 100;
+	value = value.toFixed(2);
+	return "£ " + value;
+}
+
+function formatSplit(value) {
+	if (value === "1") return value + " person";
+	return value + " people";
+}
+
+function update() {
+	let bill = Number(document.querySelector('#yourBill').value);
+	let tipPercent = document.querySelector('#tipInput').value;
+	let split = document.querySelector('#splitInput').value;
+
+	let tipValue = bill * (tipPercent / 100);
+	let tipEach = tipValue / split;
+	let newBillEach = (bill + tipValue) / split;
+
+	document.querySelector('#tipPercent').innerHTML = tipPercent + "%";
+	document.querySelector('#tipValue').innerHTML = formatMoney(tipValue);
+	document.querySelector('#totalWithTip').innerHTML = formatMoney(bill + tipValue);
+	document.querySelector('#splitValue').innerHTML = formatSplit(split);
+	document.querySelector('#billEach').innerHTML = formatMoney(newBillEach);
+	document.querySelector('#tipEach').innerHTML = formatMoney(tipEach);
+
+}
